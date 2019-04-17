97dd2f4bc054ffac35492683dffa24db7f98076d
Ken Snyder <kendsnyder@gmail.com>
2019-04-16 21:34:06 -0600

    working with useLayoutEffect()

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { WhatIsThis } from './components/WhatIsThis.js';
-import { useStateValue } from './hooks/useStateValue.js';
+import { PopOver } from './components/PopOver.js';
+import { ZIndexTop } from './components/ZIndexTop.js';
 
 export function App() {
-	const counter = useStateValue(0);
+	const [isShown1, show1] = useState(false);
+	const [isShown2, show2] = useState(false);
 
 	return (
 		<div>
+			<label>
+				<input type="checkbox" onChange={click1} /> Show Popover 1
+			</label>
+			<br />
+			<label>
+				<input type="checkbox" onChange={click2} /> Show Popover 2
+			</label>
+			<div style={{ position: 'absolute' }}>
+				{isShown1 && (
+					<ZIndexTop>
+						<PopOver top={90} left={90} />
+					</ZIndexTop>
+				)}
+				{isShown2 && (
+					<ZIndexTop>
+						<PopOver top={120} left={120} />
+					</ZIndexTop>
+				)}
+			</div>
 			<WhatIsThis />
-			<p>Change me:</p>
-			<p class="count-controls">
-				<button onClick={decrement}>-</button>
-				<span className="count-display">{counter.value}</span>
-				<button onClick={increment}>+</button>
-			</p>
-			<pre>typeof counter: {JSON.stringify(typeof counter)}</pre>
-			<pre>counter.value: {JSON.stringify(counter.value)}</pre>
-			<pre>typeof counter.set: {JSON.stringify(typeof counter.set)}</pre>
-			<pre>String(counter): {JSON.stringify(String(counter))}</pre>
-			<pre>Number(counter): {JSON.stringify(Number(counter))}</pre>
 		</div>
 	);
 
-	function decrement() {
-		return counter.set(count => count - 1);
+	function click1(event) {
+		return show1(event.target.checked);
 	}
 
-	function increment() {
-		return counter.set(count => count + 1);
+	function click2(event) {
+		return show2(event.target.checked);
 	}
 }
